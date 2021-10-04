f4e1a72ada819ba3b8b062febc24268f917e794b
Brandon <bkuon@uco.edu>
2021-10-04 18:43:55 -0500

    First commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Even_Fibonacci
+{
+    class Program
+    {
+        static long evenFib(int n)
+        {
+            if(n < 1)
+            {
+                return n;
+            }
+            if(n == 1){
+                return 2;
+            }
+
+            return ((4* evenFib(n-1)) + evenFib(n - 2));
+        }
+        static void Main(string[] args)
+        {
+            int n = 7;
+            Console.WriteLine(evenFib(n));
+        }
+    }
+}
